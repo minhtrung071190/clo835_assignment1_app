b0ce961f7dc4d9e6f99b1f69d13e1a2a0e808371
EC2 Default User <ec2-user@ip-172-31-85-174.ec2.internal>
2024-06-02 22:24:25 +0000

    Update application

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ DATABASE = os.environ.get("DATABASE") or "employees"
 COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 DBPORT = int(os.environ.get("DBPORT"))
 
+
+
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
     host= DBHOST,
